b041f73e158084dff48699113d8e3ef0a7aa80bd
NB29979 <ctwc0162@mail4.doshisha.ac.jp>
2018-05-14 19:05:18 +0900

    複数のソースファイルに対応できるように

--- ProgramChecker.py
+++ ProgramChecker.py
@@ -1,31 +1,42 @@
 import subprocess
+import os
 
 
 def exec_cmd(command):
     try:
-        res=subprocess.run(command,shell=True).stdout
-        print(res)
+        subprocess.run(command, shell=True).stdout
         return 0
     except:
         return 1
 
 
+def print_state(program_file, message):
+    print(program_file+', State: '+message)
+
+
 def check_program(program_file, answer_file):
 
-    print('State: Starting build...')
+    print_state(program_file, 'Starting build...')
     if exec_cmd('gcc '+program_file) == 1:
-        print('build exception: '+program_file)
+        print_state(program_file, 'Build Exception')
         return 1
 
-    print('State: Checking operation...')
+    print_state(program_file, 'Checking operation...')
     if exec_cmd('a.exe<input>program_tmp_output') == 1:
-        print('operation check exception: '+program_file)
+        print_state(program_file, 'Operation Check Exception')
         return 1
 
-    print('State: Checking output...')
+    print_state(program_file, 'Checking output...')
     if exec_cmd('fc program_tmp_output '+answer_file) == 1:
-        print('output check exception: '+program_file)
+        print_state(program_file, 'Output Check Exception')
         return 1
 
 
-check_program('HelloWorld.c', 'output')
+list_dir = os.listdir()
+files = [f for f in list_dir if os.path.isfile(os.path.join('.', f))]
+programs = [p for p in files if p[-2:] == '.c']
+
+print('Target programs: '+', '.join(programs))
+
+for program in programs:
+    check_program(program, 'output')
