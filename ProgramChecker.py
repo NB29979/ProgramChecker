f6625df734ffad55bdf03c19d59f41194f957153
NB29979 <ctwc0162@mail4.doshisha.ac.jp>
2018-05-14 19:24:55 +0900

    サブディレクトリにソースを移動

--- ProgramChecker.py
+++ ProgramChecker.py
@@ -14,7 +14,7 @@ def print_state(program_file, message):
     print(program_file+', State: '+message)
 
 
-def check_program(program_file, answer_file):
+def check_program(program_file, input_file, answer_file):
 
     print_state(program_file, 'Starting build...')
     if exec_cmd('gcc '+program_file) == 1:
@@ -22,7 +22,7 @@ def check_program(program_file, answer_file):
         return 1
 
     print_state(program_file, 'Checking operation...')
-    if exec_cmd('a.exe<input>program_tmp_output') == 1:
+    if exec_cmd('a.exe<'+input_file+'>program_tmp_output') == 1:
         print_state(program_file, 'Operation Check Exception')
         return 1
 
@@ -32,11 +32,12 @@ def check_program(program_file, answer_file):
         return 1
 
 
-list_dir = os.listdir()
-files = [f for f in list_dir if os.path.isfile(os.path.join('.', f))]
+list_dir = os.listdir('./kadai01')
+files = [f for f in list_dir if os.path.isfile(os.path.join('./kadai01', f))]
+print(files)
 programs = [p for p in files if p[-2:] == '.c']
 
 print('Target programs: '+', '.join(programs))
 
 for program in programs:
-    check_program(program, 'output')
+    check_program('./kadai01/'+program, './kadai01/kadai01_input', './kadai01/kadai01_output')
