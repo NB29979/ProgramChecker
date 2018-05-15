f76db7ea895c81288425fbbf0f262e24d565499f
NB29979 <ctwc0162@mail4.doshisha.ac.jp>
2018-05-15 15:33:46 +0900

    csvでidと得点が出力できるように

--- ProgramChecker.py
+++ ProgramChecker.py
@@ -1,20 +1,22 @@
+import sys
 import subprocess
 import os
 import re
+import csv
 
 
 def exec_cmd(command, failed_message):
     try:
         res = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
         if res.stderr == '':
-            print(res.stdout)
+            print(res.stdout, end='')
             return res.stdout
         else:
-            print(res.stderr)
+            print(res.stderr, end='')
             print(failed_message)
             return 1
     except subprocess.CalledProcessError:
-        print('Command Exception')
+        print('Command Exception', end='')
 
 
 def check_program(program_file, input_file, answer_file):
@@ -32,27 +34,53 @@ def check_program(program_file, input_file, answer_file):
 def disp_program(program_file):
     print('-*-*-*-*-*-')
     exec_cmd('type '+program_file, 'Program file cannot open...')
-    print('-*-*-*-*-*-\n')
+    print('\n-*-*-*-*-*-')
 
 
-target_dir = '.\\kadai01\\'
+def record_results(question, results):
+    try:
+        with open(question+'_results.csv', 'w')as csvfile:
+            writer = csv.writer(csvfile, lineterminator='\n')
+            for result in results:
+                writer.writerow(result)
+
+    except FileNotFoundError as e:
+        print(e)
+    except csv.Error as e:
+        print(e)
+
+
+question = sys.argv[1]
+results = []
+target_dir = '.\\'+question+'\\'
 list_dir = os.listdir(target_dir)
 files = [f for f in list_dir if os.path.isfile(os.path.join(target_dir, f))]
-programs = [p for p in files if p[-2:] == '.c' or p[-4:] == '.cpp']
+
+programs = [target_dir+p for p in files if p[-2:] == '.c' or p[-4:] == '.cpp']
+input_file = target_dir+'_input'
+output_file = target_dir+'_output'
 
 print('Target programs: '+', '.join(programs))
 
 for program in programs:
     score = 0
-    print('------')
-    check_program(target_dir+program, target_dir+'kadai01_input', target_dir+'kadai01_output')
-    disp_program(target_dir+program)
+    print('\n\n------^------^------^------')
+    check_program(program, input_file, output_file)
+    disp_program(program)
 
-    ID = exec_cmd('findstr /R  [0-9][0-9] '+target_dir+program, 'Finding ID Failed')
+    ID = exec_cmd('findstr /R  [0-9][0-9] '+program, 'Finding ID Failed')
     m = re.search(r'[0-9]{10}', ID)
-    if m: ID = m.group(0)
-    else: ID = 'NaN'
+    ID = m.group(0) if m else 'NaN'
 
     print('現在の学籍番号: '+ID)
-    id_input = input('学籍番号（変更しない場合Enter）>')
+    id_input = input('学籍番号（変更しない場合Enter） >')
     if id_input != '': ID = id_input
+
+    score = int(input('得点 >'))
+    print(program+' : [.c], 得点-> ', end='')
+    if program[-2:] != '.c': score -= 1
+    print(score)
+
+    results.append([ID, score])
+
+record_results(question, results)
